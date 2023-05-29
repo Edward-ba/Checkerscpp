7970a65ef26abef234737ca96168049a752e8795
Ed <ebalasingham@gmail.com>
2023-05-29 15:12:42 -0700

    [checkers] got rid of everything in checkers and moved checkers1 to checkers

--- checkers/main.cpp
+++ checkers/main.cpp
@@ -1,6 +1,62 @@
+
+
+// class King: private Piece{
+//     public:
+//         Coordinate move(Coordinate beg, dir d) {
+//             if (d == nw) {
+//                 return Coordinate(beg.get_row() - 1, beg.get_col() - 1);
+//             }
+//             if (d == ne) {
+//                 return Coordinate(beg.get_row() - 1, beg.get_col() + 1);
+//             }
+//             if (d == se) {
+//                 return Coordinate(beg.get_row() + 1, beg.get_col() + 1);
+//             }
+//             if (d == sw) {
+//                 return Coordinate(beg.get_row() + 1, beg.get_col() - 1);
+//             }
+//             return Coordinate(-1, -1);
+//         }
+//         void print() {
+//             if (get_type() == white) {
+//                 std::cout << "W";
+//             }
+//             else if (get_type() == black) {
+//                 std::cout << "B";
+//             }
+//             else {
+//                 std::cout << " ";
+//             }
+//         }
+// };
+
 #include <iostream>
-#include <vector>
+#include "./include/board.h"
+#include "./include/pawn.h"
+#include "./include/king.h"
+#include "./include/coordinate.h"
 
 int main(int argc, char* argv[]) {
-    
-};
+    Board board;
+    board.print_board(std::cout);
+
+    board.move(Coordinate(2, 2), se, white);
+
+    board.print_board(std::cout);
+
+    // {
+    //     Piece* p = new Pawn();
+    //     p->print();
+    //     p->move(Coordinate(-1, -1), ne);
+    //     delete p;
+    // }
+
+    // std::cout << "----\n";
+    // {
+    //     Piece* k = new King();
+    //     k->print();
+    //     k->move(Coordinate(-1, -1), ne);
+    //     delete k;
+    // }
+
+}
